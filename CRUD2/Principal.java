62dedad5410be5ee6c97f65986816e12b8e6ed9d
VITOR LEITE SETRAGNI <165705231+VitorSetragni@users.noreply.github.com>
2025-04-05 10:47:57 -0300

    Add files via upload

--- CRUD2/Principal.java
+++ CRUD2/Principal.java
@@ -10,12 +10,15 @@ public static void main(String[] args) {
     try {
         console = new Scanner(System.in);
         int opcao;
+        int opcao2;
         do {
 
-            System.out.println("\n\nAEDsIII");
+            System.out.println("\n\nPUCFlix");
             System.out.println("-------");
             System.out.println("> Início");
-            System.out.println("\n1 - Clientes");
+            System.out.println("\n1 - Séries");
+            System.out.println("2 - Episódios");
+            System.out.println("3 - Atores");
             System.out.println("0 - Sair");
 
             System.out.print("\nOpção: ");
@@ -24,13 +27,97 @@ public static void main(String[] args) {
             } catch(NumberFormatException e) {
                 opcao = -1;
             }
-
             switch (opcao) {
                 case 1:
-                    (new MenuClientes()).menu();
-                    break;
+                    do{
+                        System.out.println("\n\nPUCFlix");
+                        System.out.println("-------");
+                        System.out.println("> Início > Séries");
+                        System.out.println("\n1 - Incluir");
+                        System.out.println("2 - Buscar");
+                        System.out.println("3 - Alterar");
+                        System.out.println("4 - Excluir");
+                        System.out.println("0 - Retonar ao menu anterior");
+                        switch(opcao2){
+                            case 1:
+
+                            break;
+                            case 2:
+
+                            break;
+                            case 3:
+
+                            break;
+                            case 4:
+
+                            break;
+                            default:
+                                System.out.println("Opção inválida!");
+                            break;
+                        } 
+                    }while(opcao2 != 0);   
+                break;
+                case 2:
+                    do{
+                        System.out.println("\n\nPUCFlix");
+                        System.out.println("-------");
+                        System.out.println("> Início > Episódios");
+                        System.out.println("\n1 - Incluir");
+                        System.out.println("2 - Buscar");
+                        System.out.println("3 - Alterar");
+                        System.out.println("4 - Excluir");
+                        System.out.println("0 - Retonar ao menu anterior");
+                        switch(opcao2){
+                            case 1:
+
+                            break;
+                            case 2:
+
+                            break;
+                            case 3:
+
+                            break;
+                            case 4:
+
+                            break;
+                            default:
+                                System.out.println("Opção inválida!");
+                            break;
+                        }
+                    }while(opcao2 != 0);
+                break;
+                case 3:
+                    do{
+                        System.out.println("\n\nPUCFlix");
+                        System.out.println("-------");
+                        System.out.println("> Início > Atores");
+                        System.out.println("\n1 - Incluir");
+                        System.out.println("2 - Buscar");
+                        System.out.println("3 - Alterar");
+                        System.out.println("4 - Excluir");
+                        System.out.println("0 - Retonar ao menu anterior");
+                        switch(opcao2){
+                            case 1:
+
+                            break;
+                            case 2:
+
+                            break;
+                            case 3:
+
+                            break;
+                            case 4:
+
+                            break;
+                            default:
+                                System.out.println("Opção inválida!");
+                            break;
+                        }
+                    }while(opcao2 != 0);
+                break;
                 case 0:
-                    break;
+                break;
+                
                 default:
                     System.out.println("Opção inválida!");
                     break;
