c675c7b8341c50d7753aafe1ec70fca86ef0bb80
VITOR LEITE SETRAGNI <165705231+VitorSetragni@users.noreply.github.com>
2025-04-06 20:59:55 -0300

    Add files via upload

--- CRUD2/Arquivo/ArquivoSeries.java
+++ CRUD2/Arquivo/ArquivoSeries.java
@@ -1,83 +1,64 @@
 package Arquivo;
-import Entidades.Series;
 import aed3.*;
 
 public class ArquivoSeries extends aed3.Arquivo<Series> {
 
     Arquivo<Series> arqSeries;
-    HashExtensivel<ParaID> indiceIndiretoIDSerie;
-    ArvoreBMais<ParaNomeID> indiceNomeSerie;
+    HashExtensivel<ParNomeID> indiceNomeSerie;
 
-
-    public ArquivoSerieshash() throws Exception {
-        super("series", Series.class.getConstructor());
-        indiceIndiretoIDSerie = new HashExtensivel<>(
-            ParaID.class.getConstructor(), 
-            4, 
-            ".\\dados\\series\\indiceID.d.db",   // diretório
-            ".\\dados\\series\\indiceID.c.db"    // cestos 
-        );
-    }
 
     public ArquivoSeriesArvore() throws Exception {
-        super("series", Series.class.getConstructor());
+        ssuper("series", Serie.class.getConstructor());
+        
         indiceNomeSerie = new HashExtensivel<>(
-            ParaNomeID.class.getConstructor(), 
-            4, 
-            ".\\dados\\series\\indiceNome.d.db",   // diretório
-            ".\\dados\\series\\indiceNome.c.db"    // cestos 
-        );
+        ParNomeSerieId.class.getConstructor(), 
+        5, 
+        ".\\dados\\series\\indiceCPF.d.db",   // diretório
+        ".\\dados\\series\\indiceCPF.c.db"   // cestos
+        );    
     }
+    
 
     @Override
-    public int create(Cliente c) throws Exception {
+    public int create(Series c) throws Exception {
         int id = super.create(c);
-        indiceIndiretoCPF.create(new ParCPFID(c.getCpf(), id));
+        indiceNomeSerie.create(new ParnomeID(c.getnome(), id));
         return id;
     }
 
-    public Cliente read(String cpf) throws Exception {
-        ParCPFID pci = indiceIndiretoCPF.read(ParCPFID.hash(cpf));
+    public Series read(String nome) throws Exception {
+        ParnomeID pci = indiceNomeSerie.read(ParnomeID.hash(nome));
         if(pci == null)
             return null;
         return read(pci.getId());
     }
     
-    public boolean delete(String cpf) throws Exception {
-        ParCPFID pci = indiceIndiretoCPF.read(ParCPFID.hash(cpf));
+    public boolean delete(String nome) throws Exception {
+        ParnomeID pci = indiceNomeSerie.read(ParnomeID.hash(nome));
         if(pci != null) 
             if(delete(pci.getId())) 
-                return indiceIndiretoCPF.delete(ParCPFID.hash(cpf));
+                return indiceNomeSerie.delete(ParnomeID.hash(nome));
         return false;
     }
 
     @Override
     public boolean delete(int id) throws Exception {
-        Cliente c = super.read(id);
+        Series c = super.read(id);
         if(c != null) {
             if(super.delete(id))
-                return indiceIndiretoCPF.delete(ParCPFID.hash(c.getCpf()));
+                return indiceNomeSerie.delete(ParnomeID.hash(c.getnome()));
         }
         return false;
     }
 
     @Override
-    public boolean update(Cliente novoCliente) throws Exception {
-        Cliente clienteVelho = read(novoCliente.getCpf());
-        if(super.update(novoCliente)) {
-            if(novoCliente.getCpf().compareTo(clienteVelho.getCpf())!=0) {
-                indiceIndiretoCPF.delete(ParCPFID.hash(clienteVelho.getCpf()));
-                indiceIndiretoCPF.create(new ParCPFID(novoCliente.getCpf(), novoCliente.getId()));
+    public boolean update(Series novoSeries) throws Exception {
+        Series SeriesVelho = read(novoSeries.getnome());
+        if(super.update(novoSeries)) {
+            if(novoSeries.getnome().compareTo(SeriesVelho.getnome())!=0) {
+                indiceNomeSerie.delete(ParnomeID.hash(SeriesVelho.getnome()));
+                indiceNomeSerie.create(new ParnomeID(novoSeries.getnome(), novoSeries.getId()));
             }
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean serieExiste(int id) throws Exception{
-        ArquivoSeries arqSeries = new ArquivoSeries();
-        Series s = arqSeries.read(id);   // na superclasse
-        if(s != null) {
             return true;
         }
         return false;
