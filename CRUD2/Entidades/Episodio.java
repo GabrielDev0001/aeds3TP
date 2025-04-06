8e2902b8c0a3b2474e2d44af1504cf0349fd8d7e
GabrielDev0001 <gabrielhenriquemorais341@gmail.com>
2025-04-06 12:19:36 -0300

    Update Episodio.java

--- CRUD2/Entidades/Episodio.java
+++ CRUD2/Entidades/Episodio.java
@@ -4,15 +4,35 @@ public class Episodio {
     private int id;
     private String nome;
     private String dataLancamento;
-    private int duracao; 
+    private float duracao; 
     private int temporada; 
 
-    public Episodio(int id, String nome, String dataLancamento, int duracao, int temporada) {
+    public Episodio(int id, String nome, String dataLancamento, float duracao, int temporada) {
         this.id = id;
         this.nome = nome;
         this.dataLancamento = dataLancamento;
         this.duracao = duracao;
         this.temporada = temporada;
+    }
+
+    public Episodio(int id, String nome, int temporada, String dataLancamento, float duracao) throws Exception {
+        this.id = id;
+        if (nome.length() > 0 && nome.length() <= 50)
+            this.nome = nome;
+        else
+            throw new Exception("Nome inválido! Tamanho deve ser entre 1 e 50 caracteres.");
+        if (temporada >= 0)
+            this.temporada = (byte) temporada;
+        else
+            throw new Exception("Temporada inválida! Deve ser maior ou igual a zero.");
+        if (dataLancamento.length() == 10 && dataLancamento.charAt(2) == '/' && dataLancamento.charAt(5) == '/')
+            this.dataLancamento = dataLancamento;
+        else
+            throw new Exception("Data inválida! Formato deve ser dd/MM/yyyy.");
+        if (duracao >= 0)
+            this.duracao = duracao;
+        else
+            throw new Exception("Duração menor que zero!");    
     }
 
     public int getId() {
@@ -31,7 +51,7 @@ public class Episodio {
         return dataLancamento;
     }
 
-    public int getduracao() {
+    public float getduracao() {
         return duracao;
     }
 
