e7a8ea0497be243f141fc62fa1d13432f5a9474a
VITOR LEITE SETRAGNI <165705231+VitorSetragni@users.noreply.github.com>
2025-04-27 19:19:57 -0300

    Add files via upload

---
+++ TP2/Entidades/Ator.java
@@ -0,0 +1,65 @@
+package Entidades;
+
+import aed3.EntidadeArquivo;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Ator implements EntidadeArquivo{
+    private int id;
+    private String nome;
+
+    
+
+    public Ator (int id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+    public Ator () {
+        this.id = -1;
+        this.nome = "";
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public byte[] toByteArray() throws IOException {//serializa o objeto
+        // cria um ByteArrayOutputStream para armazenar os dados serializados
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+
+        dos.writeInt(id);
+        dos.writeUTF(nome);
+       
+        return baos.toByteArray();
+    }
+
+    public void fromByteArray(byte[] b) throws IOException {//deserializa o objeto
+        // cria um ByteArrayInputStream para ler os dados serializados
+        ByteArrayInputStream bais = new ByteArrayInputStream(b);
+        DataInputStream dis = new DataInputStream(bais);
+
+        id = dis.readInt();
+        nome = dis.readUTF();
+    }
+    @Override
+    public void setID(int id) {
+        this.id = id;
+    }
+    @Override
+    public int getID() {
+        return this.id;
+    }
+    
+
+}
