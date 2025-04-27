c99dd586ea277e0ddffca8d8feb50454cc7d508f
Mateus Martins Parreiras <160194896+marvnnn@users.noreply.github.com>
2025-04-27 18:18:46 -0300

    Update Ator.java

--- CRUD2/Entidades/Ator.java
+++ CRUD2/Entidades/Ator.java
@@ -1,3 +1,14 @@
+package Entidades;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+
+import aed3.Registro;
+
 public class Ator {
     private int id;
     private String nome;
@@ -10,4 +21,17 @@ public class Ator {
         this.id = -1;
         this.nome = "";
     }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+
 }
