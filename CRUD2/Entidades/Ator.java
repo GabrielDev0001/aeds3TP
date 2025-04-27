8d554805923a115c9d8f988ce65939c68ec793dc
Mateus Martins Parreiras <160194896+marvnnn@users.noreply.github.com>
2025-04-27 18:15:42 -0300

    Create Ator.java

---
+++ CRUD2/Entidades/Ator.java
@@ -0,0 +1,13 @@
+public class Ator {
+    private int id;
+    private String nome;
+
+    public Ator (int id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+    public Ator () {
+        this.id = -1;
+        this.nome = "";
+    }
+}
