5b616a667c6ced3ee30c63795cb5db6b19e194a3
Mateus Martins Parreiras <160194896+marvnnn@users.noreply.github.com>
2025-04-27 19:19:55 -0300

    Update ArquivoAtor.java

--- TP2/Arquivo/ArquivoAtor.java
+++ TP2/Arquivo/ArquivoAtor.java
@@ -5,6 +5,50 @@ import Entidades.Ator;
 import java.util.ArrayList;
 
 public class ArquivoAtor extends Arquivo<Ator>{
-    ArvoreBMais <ParIdId> indiceIdEpiIdSerie;
-    ArvoreBMais <ParIdAtor> indiceNomeEpisodio;
+    ArvoreBMais <ParIdId> indiceIdAtor;
+    ArvoreBMais <ParIdAtor> indiceNomeAtor;
+
+    public ArquivoAtor() throws Exception {
+    super("ator", Ator.class.getConstructor());
+
+        indiceNomeAtor = new ArvoreBMais<>(
+        ParIdAtor.class.getConstructor(),
+        5,
+        "./dados/" + nomeEntidade + "/indiceAtor.db");
+    }
+
+    public int criarAtor (Ator a) throws Exception{
+        int id = super.criarAtor(a);
+        indiceNomeAtor.criarAtor(new ParIdAtor(a.getNome(), id));
+        return id;
+    }
+
+    public Ator[] readNome(String nome) throws Exception{
+        ArrayList<ParIdAtor> docs = indiceNomeAtor.readNome(new ParIdAtor(nome, -1));
+        if (docs.size() > 0) {
+            Ator[] atores = new Ator[docs.size()];
+            int i = 0;
+
+            for (ParIdAtor doc : docs)
+                atores[i++] = readNome(docs.getId());
+            return atores;
+        } else
+            return null;
+    }
+
+    public boolean excluirAtor(int id) throws Exception{
+        Ator ator = readNome(id);
+        if(ator != null) {
+            if(super.excluirAtor(id)) {
+                return indiceNomeAtor.excluirAtor(new ParIdAtor(ator.getNome(), id));
+            }
+        }
+        else
+            return false;
+    }
+
+    public boolean excluirAtor(String nome, int id) {
+        
+    }
+
 }
