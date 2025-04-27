70fadda6f70bc396cc17a5dfc2a04b9b0e5bf9ef
Mateus Martins Parreiras <160194896+marvnnn@users.noreply.github.com>
2025-04-27 19:05:30 -0300

    update

---
+++ TP2/Arquivo/ArquivoAtor.java
@@ -0,0 +1,10 @@
+package Arquivo;
+import aed3;
+import Entidades.Ator;
+
+import java.util.ArrayList;
+
+public class ArquivoAtor extends Arquivo<Ator>{
+    ArvoreBMais <ParIdId> indiceIdEpiIdSerie;
+    ArvoreBMais <ParIdAtor> indiceNomeEpisodio;
+}
